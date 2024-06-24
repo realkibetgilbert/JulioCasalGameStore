f7cb6c545f941ec66931da03f5a989df1b3c35c4
Gilbert Kibet <kibetgilly354@gmail.com>
2024-06-24 07:14:13 +0300

    Define a game summary model that will be used in our application

---
+++ JulioCasalGameStore.Model/GameSummary.cs
@@ -0,0 +1,14 @@
+﻿namespace JulioCasalGameStore.Model
+{
+    public class GameSummary
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public required string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
